f71eff7606f804f3ae6eb2fbb238a2d8398fd5ff
skywarth <yigitk.ersoy@gmail.com>
2022-10-25 00:00:56 +0300

    initial commit, basic package structure and some base code

---
+++ src/laravel-config-mapper/LaravelConfigMapperServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Skywarth\LaravelConfigMapper;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class LaravelConfigMapperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $originalConfigFunc=function($str){
+            config($str);
+        };
+        foreach (glob(app_path().'/Helpers/*.php') as $filename){
+            require_once($filename);
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+
+    }
+}
