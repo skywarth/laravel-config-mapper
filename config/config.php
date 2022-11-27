a3f547ff97580f82b4a01c8d656fa54a57f52e8e
skywarth <yigitk.ersoy@gmail.com>
2022-11-27 22:47:17 +0300

    feat: differentiating folder and inside-config delimiters

--- config/config.php
+++ config/config.php
@@ -1,7 +1,28 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Folder Delimiter Character
+    |--------------------------------------------------------------------------
+    |
+    | Used separating folders/sub-dirs when generating mapped env path
+    |   Example:
+    |   --------------------
+    |   You put 'automap' as value for the config of: config/mammals/room/elephant->permissions->allowed_to_walk (file path is config/mammals/room/elephans.php)
+    |   If your folder_delimiter_character is '.', then generated env key will be: "MAMMALS.ROOM.ELEPHANT.PERMISSIONS.ALLOWED_TO_WALK"
+    |   If your folder_delimiter_character is '@', then generated env key will be: "MAMMALS@ROOM@ELEPHANT.PERMISSIONS.ALLOWED_TO_WALK"
+    |   --------------------
+    | Rules:
+    |   - Non alpha-numeric characters (except those that could mess up regex)
+    |   - Has to be char (single character string)
+    |   - Suggested values: '.','/','@','-','#','|',')'
+    |   - Cannot have the same value with `word_delimiter_character`
+    |
+    */
     'folder_delimiter_character' => '.',
+    'inside_config_delimiter_character' => '.',
     'word_delimiter_character' => '_',
     'register_helpers'=>true,
     // other options...
--- src/laravel-config-mapper/ConfigMapper.php
+++ src/laravel-config-mapper/ConfigMapper.php
@@ -16,8 +16,15 @@ class ConfigMapper
      */
     public function __construct(array $configuration)
     {
-        if($configuration['folder_delimiter_character']===$configuration['word_delimiter_character']){
-            throw new InvalidArgumentException('Folder delimiter and word delimiter cannot be the same');
+        if(
+            $configuration['folder_delimiter_character']===$configuration['word_delimiter_character']
+            ||
+            $configuration['inside_config_delimiter_character']===$configuration['word_delimiter_character']
+        ){
+            throw new InvalidArgumentException('folder_delimiter_character or inside_config_delimiter_character cannot be the same as word_delimiter_character !');
+        }
+        if(strlen($configuration['folder_delimiter_character'])!==1 || strlen($configuration['inside_config_delimiter_character'])!==1){
+            throw new InvalidArgumentException('folder_delimiter_character and inside_config_delimiter_character configs has to be char ! (Single character string)');
         }
         $this->configuration = $configuration;
     }
@@ -39,6 +46,17 @@ class ConfigMapper
     public function getFolderDelimiterCharacter(): string
     {
         return $this->configuration['folder_delimiter_character'];
+    }
+
+    /**
+     * Get the inside config delimiter character defined by the configuration.
+     * Inside config delimiter character is used for separating arrays and keys INSIDE the config, descending in the config.
+     *
+     * @return string
+     */
+    public function getInsideConfigDelimiterCharacter(): string
+    {
+        return $this->configuration['inside_config_delimiter_character'];
     }
 
     /**
@@ -65,18 +83,55 @@ class ConfigMapper
     }
 
     public function getMappedEnvKey($key):string{
-        $key=$this->replaceFolderDelimiters($key);
+        $configPathSeparated=$this->separateConfigPath($key);
+        $foldersDelimiterApplied=$this->replaceFolderDelimiters($configPathSeparated['folder_only_path']);
+        $insideConfigDelimiterApplied=$this->replaceInsideConfigDelimiters($configPathSeparated['inside_config_path']);
+        $key=$foldersDelimiterApplied.$this->getInsideConfigDelimiterCharacter().$insideConfigDelimiterApplied;
         $key=$this->replaceWordDelimiters($key);
         return strtoupper($key);
+    }
+
+
+    private function separateConfigPath($key):array{
+        //hold up this is kinda duplicate function, and even written worse. check getConfigFilePathFromConfigKeyString()
+        $exploded=explode('.',$key);
+        $configDir=config_path().'/';//every config must be under /config/
+        $folderOnlyPath='';
+        $insideConfigPath='';
+        foreach ($exploded as $pathFragment){
+            // careful, as of now of writing, File::exists returns true if the given path is FOLDER.
+            // If this changes, we should also check for folder as well
+            $folderOnlyPathFS=str_replace('.','/',$folderOnlyPath);//file system path
+            $lookupTarget=$configDir.$folderOnlyPathFS.$pathFragment;
+            $isFolderOrFile=File::exists($lookupTarget)
+                || File::exists($lookupTarget.'.php');
+            if($isFolderOrFile){
+                $folderOnlyPath.=$pathFragment.'.';
+            }else{
+                $insideConfigPath.=$pathFragment.'.';
+            }
+        }
+        $folderOnlyPath=substr($folderOnlyPath,0,-1);
+        $insideConfigPath=substr($insideConfigPath,0,-1);
+        return [
+            'folder_only_path'=>$folderOnlyPath,
+            'inside_config_path'=>$insideConfigPath
+        ];
     }
 
     private function replaceFolderDelimiters($key){
         return str_replace('.',$this->getFolderDelimiterCharacter(),$key);
     }
 
+    private function replaceInsideConfigDelimiters($key){
+        return str_replace('.',$this->getInsideConfigDelimiterCharacter(),$key);
+    }
+
     private function replaceWordDelimiters($key){
         $key=Utility::camelToSnake($key);
-        return preg_replace("/[^a-zA-Z0-9{$this->getFolderDelimiterCharacter()}]/", $this->getWordDelimiterCharacter(), $key);
+        //how are '\\' working ??? Welp, as long as it does the job...
+        //I would really appreciate it if some regex pro would provide a better alternative to the query below
+        return preg_replace("/[^a-zA-Z0-9\\{$this->getFolderDelimiterCharacter()}\\{$this->getInsideConfigDelimiterCharacter()}]/", $this->getWordDelimiterCharacter(), $key);
     }
 
 
